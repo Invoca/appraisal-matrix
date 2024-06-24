200fc3f344261160fd11f9ee1cc31ae93f4934a1
Drew Caddell <dcaddell@invoca.com>
2024-06-24 15:08:36 -0700

    bundle gem commit

---
+++ lib/appraisal/matrix.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "matrix/version"
+
+module Appraisal
+  module Matrix
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/appraisal/matrix_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Appraisal::Matrix do
+  it "has a version number" do
+    expect(Appraisal::Matrix::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
